0f16a86688721d1ae07d25ab00e25adef2f399b1
Usman Rizwan <usmanrizwan771@gmail.com>
2026-01-10 00:15:18 +0500

    Add Tavily web search tool integration to workflow
    Integrated the TavilySearch tool for web search capabilities and updated the workflow to support tool usage via a ToolNode. The agent now conditionally invokes tools based on user queries, and a system prompt instructs the assistant to use tools only for specific real-time data requests.

--- index.js
+++ index.js
@@ -7,27 +7,42 @@ import {
 import { ChatGroq } from "@langchain/groq";
 import dotenv from "dotenv";
 import readline from "node:readline/promises";
+import { TavilySearch } from "@langchain/tavily";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 
 dotenv.config();
+
+const webSearchTool = new TavilySearch({
+  topic: "general",
+  maxResults: 3,
+});
+const tools = [webSearchTool];
 
 const llmModel = new ChatGroq({
   model: "llama-3.1-8b-instant",
   temperature: 0,
-});
+}).bindTools(tools);
 
 const callModel = async (state) => {
   console.log("Calling LLM Model");
   const response = await llmModel.invoke(state.messages);
-
-  return {
-    messages: [response],
-  };
+  return { messages: [response] };
 };
+
+const toolNode = new ToolNode(tools);
+
+function shouldContinue(state) {
+  const lastMessage = state.messages.at(-1);
+  if (lastMessage?.tool_calls?.length > 0) return "tools";
+  return END;
+}
 
 const workflow = new StateGraph(MessagesAnnotation)
   .addNode("agent", callModel)
+  .addNode("tools", toolNode)
   .addEdge(START, "agent")
-  .addEdge("agent", END);
+  .addEdge("tools", "agent")
+  .addConditionalEdges("agent", shouldContinue);
 
 const app = workflow.compile();
 
@@ -41,7 +56,6 @@ const main = async (id = "1") => {
 
   while (true) {
     const user_query = await rl.question("You: ");
-
     if (user_query.toLowerCase() === "exit") {
       rl.close();
       break;
@@ -49,11 +63,23 @@ const main = async (id = "1") => {
 
     const result = await app.invoke(
       {
-        messages: [{ role: "user", content: user_query }],
+        messages: [
+          {
+            role: "system",
+            content: `
+    You are a strict assistant.
+    Only use tools when the user asks for:
+    - current weather
+    - latest news
+    - real-time data
+    For other questions, answer directly without tools.
+`,
+          },
+
+          { role: "user", content: user_query },
+        ],
       },
-      {
-        configurable: { threadId: id },
-      }
+      { configurable: { threadId: id } }
     );
 
     const lastMessage = result.messages[result.messages.length - 1];
